a9cc02a7affa97f30569c55dbb4316fbf9a35d7c
Nathan L Smith <nlloyds@gmail.com>
2012-04-18 14:39:34 -0500

    add request code commented out

--- lib/capistrano/cloudflare.rb
+++ lib/capistrano/cloudflare.rb
@@ -1,10 +1,23 @@
 require 'capistrano'
 require 'capistrano/cloudflare/version'
+require 'net/http'
 
 module Capistrano
   module CloudFlare
     def self.send_request(options = {})
+#      uri = URI('https://www.cloudflare.com/api_json.html')
+      #http = Net::HTTP.new(uri.host, uri.port)
+      #http.use_ssl = true
+      #request = Net::HTTP::Post.new(uri.request_uri)
+      #request.set_form_data({
+        #:v     => 1,
+        #:a     => 'fpurge_ts',
+        #:z     => options[:domain],
+        #:tkn   => options[:api_key],
+        #:email => options[:email]
 
+      #})
+      #response = JSON.parse(http.request(request).body)
     end
 
     def self.load_into(configuration)
